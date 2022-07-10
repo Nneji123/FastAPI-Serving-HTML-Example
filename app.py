4d8bfe76503da1268381d572ad14eaabfb2b70ff
Nneji123 <ifeanyinneji777@gmail.com>
2022-07-10 10:28:37 +0100

    add files

---
+++ app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from starlette.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+from model import interest
+app = FastAPI()
+templates = Jinja2Templates(directory='templates/')
+app.mount('/static', StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+def form_post(request: Request):
+    result = "Enter your name"
+    return templates.TemplateResponse(
+        "index.html", context={"request": request, "result": result}
+    )
+
+
+@app.post('/')
+def form_post(request: Request, name: str = Form(...), rate: int = Form(...),  principal: int = Form(...),  time: int = Form(...)):
+    value = interest(principal, rate, time)
+    tot = value + principal
+    result = f"{name.title()}, this is the interest you owe: {value}$ and the total amount you owe is {tot}$"
+    return templates.TemplateResponse('result.html', context={"request": request, 'result': result, 'rate': rate, 'principal': principal, 'time': time})
---
+++ model.py
@@ -0,0 +1,5 @@
+def interest(p, r, t):
+
+    inter = ((p*r*t)/100)
+
+    return inter
